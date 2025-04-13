4005c276d86c826d1df11139a9d3eb67c1c8a9d1
Aj242005 <akshitjain6661@gmail.com>
2025-04-14 01:36:03 +0530

    updated category in the search , for api calling

--- fetchingNews.js
+++ fetchingNews.js
@@ -16,7 +16,7 @@ const BASE_URL = "https://newsdata.io/api/1/news";
 export const newsFetching = async () => {
     try {
         const url = `https://newsdata.io/api/1/news?apikey=${process.env.NEWS_DATA_IO_API}&q=semiconductors%20OR%20nanotechnology%20OR%20physics%20OR%20chemistry%20OR%20silicon&language=en&category=science `;
-        console.log("Fetching from URL:", url);
+        //console.log("Fetching from URL:", url);
 
         const res = await fetch(url);
         const data = await res.json();
